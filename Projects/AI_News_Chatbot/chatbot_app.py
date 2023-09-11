12ae9c751a0cec167c0c1c9d6aa8853e62095565
DJ Papzin <L.fanampe@gmail.com>
2023-09-11 17:39:38 +0200

    Completed: Build a Question Answering Chatbot over Documents with Sources

---
+++ Projects/AI_News_Chatbot/chatbot_app.py
@@ -0,0 +1,114 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+import requests
+from newspaper import Article
+import time
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import DeepLake
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain import OpenAI
+
+load_dotenv()
+
+# Access the keys from the .env file
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+ACTIVELOOP_TOKEN = os.getenv('ACTIVELOOP_TOKEN')
+
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+}
+
+article_urls = [
+    "https://www.artificialintelligence-news.com/2023/05/16/openai-ceo-ai-regulation-is-essential/",
+    "https://www.artificialintelligence-news.com/2023/05/15/jay-migliaccio-ibm-watson-on-leveraging-ai-to-improve-productivity/",
+    "https://www.artificialintelligence-news.com/2023/05/15/iurii-milovanov-softserve-how-ai-ml-is-helping-boost-innovation-and-personalisation/",
+    "https://www.artificialintelligence-news.com/2023/05/11/ai-and-big-data-expo-north-america-begins-in-less-than-one-week/",
+    "https://www.artificialintelligence-news.com/2023/05/02/ai-godfather-warns-dangers-and-quits-google/",
+    "https://www.artificialintelligence-news.com/2023/04/28/palantir-demos-how-ai-can-used-military/"
+]
+
+session = requests.Session()
+pages_content = [] # where we save the scraped articles
+
+for url in article_urls:
+    try:
+        time.sleep(2) # sleep two seconds for gentle scraping
+        response = session.get(url, headers=headers, timeout=10)
+
+        if response.status_code == 200:
+            article = Article(url)
+            article.download() # download HTML of webpage
+            article.parse() # parse HTML to extract the article text
+            pages_content.append({ "url": url, "text": article.text })
+        else:
+            print(f"Failed to fetch article at {url}")
+    except Exception as e:
+        print(f"Error occurred while fetching article at {url}: {e}")
+
+embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+# TODO: use your organization id here. (by default, org id is your username)
+my_activeloop_org_id = "djpapzin"
+my_activeloop_dataset_name = "langchain_course_qabot_with_source"
+dataset_path = f"hub://{my_activeloop_org_id}/{my_activeloop_dataset_name}"
+
+db = DeepLake(dataset_path=dataset_path, embedding_function=embeddings)
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+all_texts, all_metadatas = [], []
+for d in pages_content:
+    chunks = text_splitter.split_text(d["text"])
+    for chunk in chunks:
+        all_texts.append(chunk)
+        all_metadatas.append({ "source": d["url"] })
+
+db.add_texts(all_texts, all_metadatas)
+
+llm = OpenAI(model_name="text-davinci-003", temperature=0)
+
+chain = RetrievalQAWithSourcesChain.from_chain_type(llm=llm,
+                                                    chain_type="stuff",
+                                                    retriever=db.as_retriever())
+
+d_response = chain({"question": "What does Geoffrey Hinton think about recent trends in AI?"})
+
+# Streamlit app
+# ... [Your existing imports and setup code]
+
+# Streamlit app
+def main():
+    st.title("AI News Chatbot")
+    
+    # Get user input
+    user_input = st.chat_input("Ask me anything about recent AI news")
+    
+    if user_input:
+        # Display a status message while processing the user's input
+        status_message = st.status("Processing your request...")
+        
+        # Get chatbot's response
+        d_response = chain({"question": user_input})
+        
+        # Update or replace the status message
+        status_message.empty()  # This line clears the status message
+        
+        # Display user's message
+        with st.chat_message("user"):
+            st.write(user_input)
+        
+        # Display chatbot's response
+        with st.chat_message("assistant"):
+            st.write(d_response["answer"])
+            
+            # Display sources
+            st.write("Sources:")
+            for source in d_response["sources"].split(", "):
+                st.write("- " + source)
+
+if __name__ == "__main__":
+    main()

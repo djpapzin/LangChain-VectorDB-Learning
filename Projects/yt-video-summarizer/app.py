024b44aef422c3bc31d34990be8f85bf5e7cd186
DJ Papzin <L.fanampe@gmail.com>
2023-10-05 14:49:40 +0200

    aider: Refactored video download and transcription process, added error handling for video quality selection.
    # Aider chat conversation:
    
    ASSISTANT: No changes are needed.

--- Projects/yt-video-summarizer/app.py
+++ Projects/yt-video-summarizer/app.py
@@ -29,7 +29,7 @@ def download_mp4_from_youtube(url):
         result = ydl.extract_info(url, download=False)
 
     # Parse and display available video quality options
-    available_formats_str = []
+    available_formats_str = ["Select preferred video format"]
     available_formats = []
     for format in result['formats']:
         if format['ext'] == 'mp4' and 'height' in format:
@@ -42,15 +42,18 @@ def download_mp4_from_youtube(url):
     selected_quality = st.radio("Select video quality:", available_formats_str, index=None)
 
     # Download the video based on the selected quality
-    selected_format = available_formats[available_formats_str.index(selected_quality)]
-    ydl_opts = {
-        'format': selected_format['format_id'],
-        'outtmpl': '%(title)s.mp4',
-        'quiet': True,
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        result = ydl.extract_info(url, download=True)
-        video_title = result['title']
+    if selected_quality != "Select preferred video format":
+        selected_format = available_formats[available_formats_str.index(selected_quality)]
+        ydl_opts = {
+            'format': selected_format['format_id'],
+            'outtmpl': '%(title)s.mp4',
+            'quiet': True,
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            result = ydl.extract_info(url, download=True)
+            video_title = result['title']
+    else:
+        video_title = None
 
     return video_title, selected_quality
 
@@ -62,59 +65,62 @@ if st.button("Start"):
     if url:
         video_title, selected_quality = download_mp4_from_youtube(url)
 
-        # Show progress updates to the user
-        progress_bar = st.progress(0)
-        status_text = st.empty()
+        if video_title:
+            # Show progress updates to the user
+            progress_bar = st.progress(0)
+            status_text = st.empty()
 
-        # Load the Whisper model for transcription
-        model = whisper.load_model("tiny")
-        result = model.transcribe(f"{video_title}.mp4")
+            # Load the Whisper model for transcription
+            model = whisper.load_model("tiny")
+            result = model.transcribe(f"{video_title}.mp4")
 
-        # Extract transcription from the result
-        transcription = result['text']
+            # Extract transcription from the result
+            transcription = result['text']
 
-        # Save the transcription to a file
-        with open(f'{video_title}_transcription.txt', 'w') as file:
-            file.write(transcription)
+            # Save the transcription to a file
+            with open(f'{video_title}_transcription.txt', 'w') as file:
+                file.write(transcription)
 
-        # Initialize the LangChain model for summarization
-        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+            # Initialize the LangChain model for summarization
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
-        # Split the transcription text into manageable chunks
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
-        )
-        with open(f'{video_title}_transcription.txt') as f:
-            text = f.read()
-        texts = text_splitter.split_text(text)
-        docs = [Document(page_content=t) for t in texts[:4]]
-
-        # Use the 'refine' summarization chain with the custom prompt
-        chain = load_summarize_chain(llm, chain_type="refine")
-        output_summary = chain.run(docs)
-        wrapped_text = textwrap.fill(output_summary, width=100)
-
-        # Display the refined summary
-        st.subheader("Summary:")
-        st.write(wrapped_text)
-
-        # Button to download transcript
-        if st.button("Download Transcript"):
-            st.download_button(
-                label="Download Transcript",
-                data=f'{video_title}_transcription.txt',
-                file_name=f'{video_title}_transcription.txt',
+            # Split the transcription text into manageable chunks
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
             )
+            with open(f'{video_title}_transcription.txt') as f:
+                text = f.read()
+            texts = text_splitter.split_text(text)
+            docs = [Document(page_content=t) for t in texts[:4]]
 
-        # Button to download summary
-        if st.button("Download Summary"):
-            st.download_button(
-                label="Download Summary",
-                data=wrapped_text,
-                file_name=f'{video_title}_summary.txt',
-            )
+            # Use the 'refine' summarization chain with the custom prompt
+            chain = load_summarize_chain(llm, chain_type="refine")
+            output_summary = chain.run(docs)
+            wrapped_text = textwrap.fill(output_summary, width=100)
 
-        # Expand icon to display the rest of the transcription
-        with st.expander("Transcription", expanded=True):
-            st.write(transcription)
-    
+            # Display the refined summary
+            st.subheader("Summary:")
+            st.write(wrapped_text)
+
+            # Button to download transcript
+            if st.button("Download Transcript"):
+                st.download_button(
+                    label="Download Transcript",
+                    data=f'{video_title}_transcription.txt',
+                    file_name=f'{video_title}_transcription.txt',
+                )
+
+            # Button to download summary
+            if st.button("Download Summary"):
+                st.download_button(
+                    label="Download Summary",
+                    data=wrapped_text,
+                    file_name=f'{video_title}_summary.txt',
+                )
+
+            # Expand icon to display the rest of the transcription
+            with st.expander("Transcription", expanded=True):
+                st.write(transcription)
+        else:
+            st.write("Please select a video quality.")
+    ```

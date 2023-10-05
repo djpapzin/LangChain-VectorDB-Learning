4e20bafb785aa2d681408d61c6a2986e7d151b5d
DJ Papzin <L.fanampe@gmail.com>
2023-10-05 13:59:10 +0200

    aider: Refactored app.py to add a Streamlit interface, display summary and transcription, and provide download buttons for transcript and summary.
    # Aider chat conversation:
    
    ASSISTANT: No changes are needed.

--- Projects/yt-video-summarizer/app.py
+++ Projects/yt-video-summarizer/app.py
@@ -1,6 +1,6 @@
-# Import necessary libraries and modules
 import os
 from dotenv import load_dotenv
+import streamlit as st
 import yt_dlp
 import whisper
 from langchain.chat_models import ChatOpenAI
@@ -15,12 +15,15 @@ import textwrap
 # Load environment variables (API keys in this case)
 load_dotenv()
 
+# Set the title of the app
+st.title("Youtube Video Summarizer")
+
 # Retrieve and set API keys from environment variables
 openai_api_key = os.environ['OPENAI_API_KEY']
 activeloop_token = os.environ['ACTIVELOOP_TOKEN']
 
 # Function to download MP4 videos from YouTube
-def download_mp4_from_youtube(url):
+def download_mp4_from_youtube(url, selected_quality):
     # Define options to list available video formats
     ydl_opts = {
         'listformats': True,
@@ -30,17 +33,15 @@ def download_mp4_from_youtube(url):
         result = ydl.extract_info(url, download=False)
 
     # Parse and display available video quality options
-    print("Available video quality options:")
     available_formats = set()
     for format in result['formats']:
         if format['ext'] == 'mp4' and 'height' in format:
             height = format['height']
             available_formats.add(height)
     available_formats_str = [str(height) + 'p' for height in sorted(available_formats)]
-    print(', '.join(available_formats_str))
 
     # Get user input for desired video quality
-    selected_quality = input("Enter the desired video quality: ")
+    selected_quality = st.radio("Select video quality:", available_formats_str)
 
     # Define options for video download based on user's choice
     ydl_opts = {
@@ -57,18 +58,21 @@ def download_mp4_from_youtube(url):
     return video_title
 
 # Get YouTube video URL from user
-url = input("Enter the YouTube video URL: ")
-video_title = download_mp4_from_youtube(url)
+url = st.text_input("Enter the YouTube video URL: ")
+selected_quality = None
+
+if url:
+    selected_quality = download_mp4_from_youtube(url, selected_quality)
 
 # Load the Whisper model for transcription
 model = whisper.load_model("tiny")
-result = model.transcribe(f"{video_title}.mp4")
+result = model.transcribe(f"{selected_quality}.mp4")
 
 # Extract transcription from the result
 transcription = result['text']
 
 # Save the transcription to a file
-with open(f'{video_title}_transcription.txt', 'w') as file:
+with open(f'{selected_quality}_transcription.txt', 'w') as file:
     file.write(transcription)
 
 # Initialize the LangChain model for summarization
@@ -78,24 +82,36 @@ llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
 )
-with open(f'{video_title}_transcription.txt') as f:
+with open(f'{selected_quality}_transcription.txt') as f:
     text = f.read()
 texts = text_splitter.split_text(text)
 docs = [Document(page_content=t) for t in texts[:4]]
-
-# # Define the prompt for the 'refine' summarization chain
-# prompt_template = """Write a concise bullet point summary of the following:
-# {text}
-# CONSCISE SUMMARY IN BULLET POINTS:"""
-# BULLET_POINT_PROMPT = PromptTemplate(template=prompt_template, 
-#                         input_variables=["text"])
-# chain = load_summarize_chain(llm, chain_type="refine", refine_prompt=BULLET_POINT_PROMPT)
 
 # Use the 'refine' summarization chain with the custom prompt
 chain = load_summarize_chain(llm, chain_type="refine")
 output_summary = chain.run(docs)
 wrapped_text = textwrap.fill(output_summary, width=100)
 
-# Print the refined summary
-print("\nSummary:")
-print(wrapped_text)
+# Display the refined summary
+st.subheader("Summary:")
+st.write(wrapped_text)
+
+# Button to download transcript
+if st.button("Download Transcript"):
+    st.download_button(
+        label="Download Transcript",
+        data=f'{selected_quality}_transcription.txt',
+        file_name=f'{selected_quality}_transcription.txt',
+    )
+
+# Button to download summary
+if st.button("Download Summary"):
+    st.download_button(
+        label="Download Summary",
+        data=wrapped_text,
+        file_name=f'{selected_quality}_summary.txt',
+    )
+
+# Expand icon to display the rest of the transcription
+st.expander("Transcription", expanded=True):
+    st.write(transcription)

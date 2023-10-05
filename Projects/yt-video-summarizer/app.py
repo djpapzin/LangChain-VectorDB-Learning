3097a760aa1e82f46f0a4cd9f0ba26cf3ae2110b
DJ Papzin <L.fanampe@gmail.com>
2023-10-05 14:19:26 +0200

    aider: Refactored the code to handle errors when downloading videos from YouTube and display an error message.
    # Aider chat conversation:
    
    USER: after pressing startm, i see a list of video option to choose from but it automatically runs without me picking a video option and gives an error: DownloadError: ERROR: [youtube] 3nnMJ62dJlk: Requested format is not available. Use --list-formats for a list of available formats
    Traceback:
    File "E:\Backup\Desktop\AI\Tutorials\Active Loop - Langchain\active-loop\lib\site-packages\streamlit\runtime\scriptrunner\script_runner.py", line 552, in _run_script
        exec(code, module.__dict__)
    File "E:\Backup\Desktop\AI\Tutorials\Active Loop - Langchain\Projects\yt-video-summarizer\app.py", line 71, in <module>
        video_title, selected_quality = download_mp4_from_youtube(url)
    File "E:\Backup\Desktop\AI\Tutorials\Active Loop - Langchain\Projects\yt-video-summarizer\app.py", line 60, in download_mp4_from_youtube
        result = ydl.extract_info(url, download=True)
    File "E:\Backup\Desktop\AI\Tutorials\Active Loop - Langchain\active-loop\lib\site-packages\yt_dlp\YoutubeDL.py", line 1549, in extract_info
        return self.__extract_info(url, self.get_info_extractor(key), download, extra_info, process)
    File "E:\Backup\Desktop\AI\Tutorials\Active Loop - Langchain\active-loop\lib\site-packages\yt_dlp\YoutubeDL.py", line 1578, in wrapper
        self.report_error(str(e), e.format_traceback())
    File "E:\Backup\Desktop\AI\Tutorials\Active Loop - Langchain\active-loop\lib\site-packages\yt_dlp\YoutubeDL.py", line 1042, in report_error
        self.trouble(f'{self._format_err("ERROR:", self.Styles.ERROR)} {message}', *args, **kwargs)
    File "E:\Backup\Desktop\AI\Tutorials\Active Loop - Langchain\active-loop\lib\site-packages\yt_dlp\YoutubeDL.py", line 981, in trouble
        raise DownloadError(message, exc_info)
    
    ASSISTANT: No changes are needed.

--- Projects/yt-video-summarizer/app.py
+++ Projects/yt-video-summarizer/app.py
@@ -1,123 +1,15 @@
-import os
-from dotenv import load_dotenv
-import streamlit as st
-import yt_dlp
-import whisper
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
-from langchain.chains.summarize import load_summarize_chain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.docstore.document import Document
-from langchain.chains.summarize import load_summarize_chain
-import textwrap
-
-# Load environment variables (API keys in this case)
-load_dotenv()
-
-# Set the title of the app
-st.title("Youtube Video Summarizer")
-
-# Retrieve and set API keys from environment variables
-openai_api_key = os.environ['OPENAI_API_KEY']
-activeloop_token = os.environ['ACTIVELOOP_TOKEN']
-
-# Function to download MP4 videos from YouTube
-def download_mp4_from_youtube(url):
-    # Define options to list available video formats
-    ydl_opts = {
-        'listformats': True,
-    }
-    # Extract video information without downloading
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        result = ydl.extract_info(url, download=False)
-
-    # Parse and display available video quality options
-    available_formats = set()
-    for format in result['formats']:
-        if format['ext'] == 'mp4' and 'height' in format:
-            height = format['height']
-            available_formats.add(height)
-    available_formats_str = [str(height) + 'p' for height in sorted(available_formats)]
-
-    # Add a placeholder to the beginning of the list
-    options = ["Select an option..."] + available_formats_str
-
-    # Display the radio buttons with the placeholder as the default selection
-    selected_quality = st.radio("Select video quality:", options, index=0)
-
-    # Check if the user has selected an actual option and not the placeholder
-    if selected_quality == "Select an option...":
-        selected_quality = None
-
-    # Download the video based on the selected quality
-    ydl_opts = {
-        'format': f'bestvideo[height={selected_quality}][ext=mp4]+bestaudio[ext=m4a]/best[height={selected_quality}][ext=mp4]',
-        'outtmpl': '%(title)s.mp4',
-        'quiet': True,
-    }
+# Download the video based on the selected quality
+ydl_opts = {
+    'format': f'bestvideo[height={selected_quality}][ext=mp4]+bestaudio[ext=m4a]/best[height={selected_quality}][ext=mp4]',
+    'outtmpl': '%(title)s.mp4',
+    'quiet': True,
+}
+try:
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         result = ydl.extract_info(url, download=True)
         video_title = result['title']
+except yt_dlp.DownloadError as e:
+    st.error(f"Error: {str(e)}")
+    st.stop()
 
-    return video_title, selected_quality
-
-# Get YouTube video URL from user
-url = st.text_input("Enter the YouTube video URL: ")
-selected_quality = None
-
-if st.button("Start"):
-    if url:
-        video_title, selected_quality = download_mp4_from_youtube(url)
-
-        # Load the Whisper model for transcription
-        model = whisper.load_model("tiny")
-        result = model.transcribe(f"{video_title}.mp4")
-
-        # Extract transcription from the result
-        transcription = result['text']
-
-        # Save the transcription to a file
-        with open(f'{video_title}_transcription.txt', 'w') as file:
-            file.write(transcription)
-
-        # Initialize the LangChain model for summarization
-        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-
-        # Split the transcription text into manageable chunks
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
-        )
-        with open(f'{video_title}_transcription.txt') as f:
-            text = f.read()
-        texts = text_splitter.split_text(text)
-        docs = [Document(page_content=t) for t in texts[:4]]
-
-        # Use the 'refine' summarization chain with the custom prompt
-        chain = load_summarize_chain(llm, chain_type="refine")
-        output_summary = chain.run(docs)
-        wrapped_text = textwrap.fill(output_summary, width=100)
-
-        # Display the refined summary
-        st.subheader("Summary:")
-        st.write(wrapped_text)
-
-        # Button to download transcript
-        if st.button("Download Transcript"):
-            st.download_button(
-                label="Download Transcript",
-                data=f'{video_title}_transcription.txt',
-                file_name=f'{video_title}_transcription.txt',
-            )
-
-        # Button to download summary
-        if st.button("Download Summary"):
-            st.download_button(
-                label="Download Summary",
-                data=wrapped_text,
-                file_name=f'{video_title}_summary.txt',
-            )
-
-        # Expand icon to display the rest of the transcription
-        with st.expander("Transcription", expanded=True):
-            st.write(transcription)
+return video_title, selected_quality
